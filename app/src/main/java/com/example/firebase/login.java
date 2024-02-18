9b46e9c6342162af706b38ecd93e45f4beed074b
Kamrul <kakamrul2000@gmail.com>
2024-02-19 02:15:10 +0600

    add

--- app/src/main/java/com/example/firebase/login.java
+++ app/src/main/java/com/example/firebase/login.java
@@ -3,15 +3,28 @@ package com.example.firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class login extends AppCompatActivity {
 
     Button Login;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Login = findViewById(R.id.btn_login_id);
+        email = findViewById(R.id.login_email_id);
+        password = findViewById(R.id.login_password_id);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
