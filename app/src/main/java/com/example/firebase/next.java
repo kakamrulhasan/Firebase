7b2ce8201b71c42095746b35af3e992afb9bed04
Kamrul <kakamrul2000@gmail.com>
2024-02-20 01:39:03 +0600

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/firebase/next.java
+++ app/src/main/java/com/example/firebase/next.java
@@ -7,9 +7,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
 public class next extends AppCompatActivity {
 
     EditText name , age;
+    FirebaseDatabase firebaseDatabase;
     Button addBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,19 @@ public class next extends AppCompatActivity {
         setContentView(R.layout.activity_next);
         name = findViewById(R.id.name_id);
         age = findViewById(R.id.age_id);
+        firebaseDatabase = FirebaseDatabase.getInstance();
         addBtn = findViewById(R.id.add_id);
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Here we using real time database
+                //firebaseDatabase.getReference().child("vendor1").setValue("rani");
+                HashMap<String,Object> m = new HashMap<>();
+                m.put("name",name.getText().toString());
+                m.put("age",age.getText().toString());
+                firebaseDatabase.getReference().child("jax").push().setValue(m);
+                //we are using push() for multiple values
+
 
             }
         });
